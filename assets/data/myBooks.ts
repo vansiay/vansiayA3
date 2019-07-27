9f25e6374aa7f939e0a1af26736867a8ab2256e8
vansiay <52579454+vansiay@users.noreply.github.com>
2019-07-27 00:49:13 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import {Books} from '../../app/myBookInfo';
+
+export const myBooks : Books[] =
+[
+    {bkname : 'The Great Gatsby',bkauthor : 'F. Scott Fitzgerald',bkgenre : 'Noval',yearofPublish: 'April 10, 1925',bkImg : 'TheGreatG.jpeg'},
+    {bkname : 'To Kill A Mockingbird',bkauthor : 'Harper Lee',bkgenre : 'Southern Gothic',yearofPublish: 'July 11, 1960',bkImg : 'Mockingbird.jpeg'},
+    {bkname : 'The Catcher in the Rye',bkauthor : 'J. D. Salinger',bkgenre : 'Realistic fiction',yearofPublish: 'July 16, 1951',bkImg : 'Rye.jpg'},
+    {bkname : 'The Hobbit',bkauthor : 'J. R. R. Tolkien',bkgenre : 'High Fantasy',yearofPublish: 'September 21, 1937',bkImg : 'TheHobbit.jpg'}
+]
